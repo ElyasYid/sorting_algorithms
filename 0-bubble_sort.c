93fcd10b549e54e4b035dd7fed11fd6d7ddfe7e9
ElyasYid <elyassayle27@gmail.com>
2024-01-18 05:29:17 -0800

    olo

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - function uses bubble sort
+ * @array: array to sort
+ * @n: size of array
+ * Return: Nothing
+*/
+
+void bubble_sort(int *array, size_t n)
+{
+	size_t i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int temp;
+
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, n);
+			}
+		}
+	}
+}
