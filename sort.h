3e3a52f8d851e09f6d6b66cb630778bcfbefe0b5
ElyasYid <elyassayle27@gmail.com>
2024-01-23 03:38:57 -0800

    grand

--- sort.h
+++ sort.h
@@ -2,9 +2,6 @@
 #define SORT_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
 
 
 /**
@@ -14,6 +11,7 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
     const int n;
@@ -24,19 +22,13 @@ typedef struct listint_s
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
+void quicksort(int *array, size_t size, ssize_t low, ssize_t high);
 void quick_sort(int *array, size_t size);
-void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
-
+void swap(int *array, size_t size, int *x, int *y);
+size_t lomuto_partition(int *array, size_t size, ssize_t low, ssize_t high);
+void bubble_sort(int *array, size_t n);
+void insertion_sort_list(listint_t **list);
+void swap(listint_t *x, listint_t *y);
+void selection_sort(int *array, size_t n);
 
 #endif
